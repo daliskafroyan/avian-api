821da7db3dc5c20bb93f21123cce34336b774397
Daliska F. Royan <firsta@ritase.com>
2023-07-31 10:39:53 +0700

    feat: move port to env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3030);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
